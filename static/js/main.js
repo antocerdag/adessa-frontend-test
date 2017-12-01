ba40f55ba6b3b5bb0fe41083d88add9023a49d8a
Antonia Cerda <antoniacerdag@gmail.com>
2017-12-01 12:52:02 -0300

    add and remove

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 	catalogo.forEach(function(e){
-		console.log(e);
-
 		var img = e.imageURL;
 		var name = e.name;
 		var price = e.price;
@@ -11,9 +9,20 @@ $(document).ready(function() {
 		$(".row-products").append("<div class='col-md-2 center-align'><div class='productbox justify-content-center'><div class='products'><img class='img-responsive' src='"+img+"'></div><p class='productinfo text-center'>"+name+"</p><p class='price text-center'>"+currency +price+"</p><a class='btnadd'><button type='button' class='btn btn-success'>Add to car</button></a></div>")
 
 		$(".btnadd").click(function() {
-		/* Act on the event */
-			$(".carproducts").append("<div class='col-md-2 center-align'><div class='productbox justify-content-center'><div class='products'><img class='img-responsive' src='"+img+"'></div><p class='productinfo text-center'>"+name+"</p><p class='price text-center'>"+currency +price+"</p><a class='btnadd'><button type='button' class='btn btn-success'>Add to car</button></a></div>")
+		//al hacer click en el boton agregar, agrega a la lista del carro el producto
+			
+			var product = $(this).parent();
+			$(".rowcar").append(product);
+			$(".btn-success").parent().html("<a class='btnremove'><button type='button' class='btn btn-light'>Remove</button></a>"); //INTENTO CAMBIAR TEXTO DE BOTÓN
+
+			$(".btn").click(function(event) {
+			
+			var item= $(this).parent();
+         	$(item).remove();
+         	console.log("Removed");
+			});
 		});
+
 	})
 
 
