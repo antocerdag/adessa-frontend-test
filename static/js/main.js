13b6f87ae877513eba9d6da6bf9ef5e746ff1db2
Antonia Cerda <antoniacerdag@gmail.com>
2017-12-01 11:57:51 -0300

    adding function to button add

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,14 @@ $(document).ready(function() {
 		var price = e.price;
 		var currency = e.currency;
 		var id = e.id;
+		//Coloco toda la información de los productos dentro de un div
+		$(".row-products").append("<div class='col-md-2 center-align'><div class='productbox justify-content-center'><div class='products'><img class='img-responsive' src='"+img+"'></div><p class='productinfo text-center'>"+name+"</p><p class='price text-center'>"+currency +price+"</p><a class='btnadd'><button type='button' class='btn btn-success'>Add to car</button></a></div>")
 
-		$(".row-products").append("<div class='col-md-2 '><div class='productbox'><div class='products'><img class='img-responsive' src='"+img+"'></div></div></div>")
+		$(".btnadd").click(function() {
+		/* Act on the event */
+			$(".carproducts").append("<div class='col-md-2 center-align'><div class='productbox justify-content-center'><div class='products'><img class='img-responsive' src='"+img+"'></div><p class='productinfo text-center'>"+name+"</p><p class='price text-center'>"+currency +price+"</p><a class='btnadd'><button type='button' class='btn btn-success'>Add to car</button></a></div>")
+		});
 	})
+
+
 });
